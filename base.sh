5d3cba8e9254d2b20405c4541faa884d1ed05f75
imzhucn <43346552+imzhucn@users.noreply.github.com>
2020-02-23 02:15:50 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,7 +2,7 @@ apt-get -y install wget
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
-echo root:hbwhsj|chpasswd
+echo root:admin|chpasswd
 echo alias ls='ls --color' >>~/.bashrc
 echo alias ll='ls -l --color=auto' >>~/.bashrc
 echo alias grep='grep --color=auto' >>~/.bashrc
@@ -10,3 +10,4 @@ source ~/.bashrc
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh 
 wget --no-check-certificate https://git.io/v2ray.sh && chmod +x v2ray.sh 
 ll
+passwd
