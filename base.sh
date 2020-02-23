7f3c4bbae95837fc2d261ed02a72745979faf02a
imzhucn <43346552+imzhucn@users.noreply.github.com>
2020-02-24 01:01:50 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,4 +1,4 @@
-apt-get -y install wget curl 
+apt-get -y install wget curl xz-utils
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
@@ -9,5 +9,6 @@ echo "alias grep='grep --color=auto'" >>~/.bashrc
 source ~/.bashrc
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh 
 wget --no-check-certificate https://git.io/v2ray.sh && chmod +x v2ray.sh 
+wget --no-check-certificate https://github.com/V2RaySSR/Trojan/raw/master/Trojan.sh && chmod +x Trojan.sh 
 ls -l --color=auto
 passwd
