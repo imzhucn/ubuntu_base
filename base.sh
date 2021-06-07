a8bde056070365d6e343db90ea2dc7567df4af42
imzhucn <43346552+imzhucn@users.noreply.github.com>
2021-06-08 00:57:58 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -11,9 +11,9 @@ echo "alias grep='grep --color=auto'" >>~/.bashrc
 source ~/.bashrc
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh 
 wget --no-check-certificate https://github.com/V2RaySSR/Trojan/raw/master/Trojan.sh && chmod +x Trojan.sh 
-mv Trojan.sh old-trojan.sh
+mv -f Trojan.sh old-trojan.sh
 wget --no-check-certificate https://git.io/trojan-install  && chmod +x trojan-install 
-mv trojan-install new-trojan.sh
+mv -f trojan-install new-trojan.sh
 wget --no-check-certificate https://git.io/trojan.txt
 
 ls -l --color=auto
@@ -24,14 +24,14 @@ hwclock --systohc
 wget https://github.com/fatedier/frp/releases/download/v0.37.0/frp_0.37.0_linux_amd64.tar.gz
 tar zxvf frp_*.tar.gz
 mkdir .frp
-mv frp_0.*linux_amd64 .frp
+mv -f frp_0.*linux_amd64 .frp
 rm -rf frp_*.tar.gz
 rm -rf .frp/frpc*
 rm -rf .frp/frps.ini
 
 wget --no-check-certificate https://raw.githubusercontent.com/imzhucn/ubuntu_base/master/frps.sh
 wget --no-check-certificate https://raw.githubusercontent.com/imzhucn/ubuntu_base/master/frps.ini
-mv frps.ini .frp/frps.ini
+mv -f frps.ini .frp/frps.ini
 wget --no-check-certificate https://raw.githubusercontent.com/imzhucn/ubuntu_base/master/frps.service
 mv -f frps.service /usr/lib/systemd/system/frps.service
 systemctl daemon-reload
