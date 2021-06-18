62b5f002e0a4c618434837257dc31f5dd8203d37
imzhucn <43346552+imzhucn@users.noreply.github.com>
2021-06-19 03:16:41 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -97,7 +97,9 @@ rm -rf web.zip
 systemctl enable nginx.service
 systemctl restart nginx
 systemctl status nginx
-
+wget https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.15.10/Clash.for.Windows-0.15.10-win.7z -O /usr/share/nginx/html/Clash.for.Windows-0.15.10-win.7z
+wget https://github.com/Kr328/ClashForAndroid/releases/download/v2.3.22/app-arm64-v8a-release.apk -O /usr/share/nginx/html/app-arm64-v8a-release.apk
+wget https://github.com/Kr328/ClashForAndroid/releases/download/v2.3.22/app-armeabi-v7a-release.apk -O /usr/share/nginx/html/app-armeabi-v7a-release.apk
 
 ##卸载阿里云盾
 echo && echo && echo
