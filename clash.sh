8dfb5450c09e57b21c9634208ec7b082c8b638d1
imzhucn <43346552+imzhucn@users.noreply.github.com>
2021-06-26 19:14:49 +0800

    Update clash.sh

--- clash.sh
+++ clash.sh
@@ -1,7 +1,21 @@
-read -p "输入名称和密码:" mima
-#echo $mima
+if [ -f hostname.txt ] ; then
+wangzhi=$(cat hostname.txt)
+echo -e "\n\n 默认网址为：$wangzhi \n 请确认是否正确 \n\n"
+
+else
+echo -e "\n hostname文件不存在 \n\n"
+sleep 2
 read -p "输入网址:" wangzhi
-cp /usr/share/nginx/html/clash.yaml "/usr/share/nginx/html/$mima.yaml"
+echo $wangzhi > hostname.txt
+
+fi
+
+read -p "输入名称和密码:" mima
+
+
+cp -rf /usr/share/nginx/html/clash.yaml "/usr/share/nginx/html/$mima.yaml"
 sed -i "s/mimamima/$mima/g" "/usr/share/nginx/html/$mima.yaml"
 sed -i "s/wangzhiwangzhi/$wangzhi/g" "/usr/share/nginx/html/$mima.yaml"
-echo http://$wangzhi/$mima.yaml
+echo -e "\n\n 链接："
+echo -e "http://$wangzhi/$mima.yaml \n\n"
+
