1b29c28ba6b2eb0c94b1b36044eb33cfd4d51417
imzhucn <43346552+imzhucn@users.noreply.github.com>
2022-04-14 18:30:36 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -7,7 +7,7 @@ export PATH
 #	Author: imzhu
 #=================================================
 
-sh_ver="1.0.8"
+sh_ver="1.0.9"
 github="raw.githubusercontent.com/imzhucn/ubuntu_base/master"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
@@ -42,7 +42,7 @@ base_timezone(){
     ##基础环境构建
     echo && echo && echo
     #echo -e "\033[1;32m 基础环境构建 \033[0m"
-    rpm -ivh http://mirrors.wlnmp.com/centos/wlnmp-release-centos.noarch.rpm
+   # rpm -ivh http://mirrors.wlnmp.com/centos/wlnmp-release-centos.noarch.rpm
 
     echo 'nameserver 8.8.8.8' >>/etc/resolv.conf 
     echo 'nameserver 8.8.4.4' >>/etc/resolv.conf 
